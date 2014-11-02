ed80502d8a121c4c06a219caaacb7bc32f756ca0
Alex Doloz <doloz.alexander@gmail.com>
2014-11-03 05:49:54 +0600

    Implemented copying of scheme

--- scheme.js
+++ scheme.js
@@ -37,6 +37,15 @@ Ball.prototype.toString = function() {
 		+ "    " + simpleBall.bottom + "    \n");
 }
 
+Ball.prototype.connectionsCount = function() {
+	var ball = this;
+	var result = 0;
+	directions.forEach(function(dir) {
+		if (ball[dir]) result++;
+	});
+	return result;
+}
+
 function Scheme(n) {
 	for (var i = 0; i < n; i++) {
 		this[i] = new Ball(i);
@@ -72,6 +81,25 @@ Scheme.prototype.toString = function() {
 	return result;
 }
 
+Scheme.prototype.connectionsCount = function() {
+	var result = 0;
+	for (var i = 0; i < this.count; i++) {
+		result += this[i].connectionsCount();
+	}
+	return result;
+}
+
+Scheme.prototype.copy = function() {
+	var copied = new Scheme(this.count);
+	for (var i = 0; i < this.count; i++) {
+		for (var j = 0; j < directions.length; j++) {
+			var dir = directions[j];
+			if (!this[i][dir]) copied[i][dir] = null;
+				else copied[i][dir] = copied[this[i][dir].self];
+		}
+	}
+	return copied;
+}
 // Scheme.prototype.getBall = function(number) {
 // 	if (number >= 0 && number < this.count) {
 // 		return this[number]
@@ -130,18 +158,33 @@ function autoFill(ball) {
 }
 
 function autoFillAll(scheme) {
-	for (var i = 0; i < scheme.count; i++) {
-		var result = autoFill(scheme[i]);
-		if (!result) return false;
-	}
+	// var initialConnectionsCount = scheme.connectionsCount()
+	do {
+		var lastConnectionsCount = scheme.connectionsCount();
+		for (var i = 0; i < scheme.count; i++) {
+			var result = autoFill(scheme[i]);
+			if (!result) return false;
+		}
+	} while (lastConnectionsCount != scheme.connectionsCount());
+
+	// if (scheme.connectionsCount != initialConnectionsCount) {
+	// 	return autoFillAll(scheme);
+	// }
 	return true;
+
 }
 
 var s = new Scheme(5);
 s[2].left = s[1];
 s[2].top = s[3];
 s[1].top = s[4];
+
+var s2 = s.copy();
+s2[1].top = s2[1];
 // s[3].left = s[4];
 // s[1].right = s[0];
 var result = autoFillAll(s);
 console.log(s.toString());
+console.log(s2.toString());
+
+// console.log(s.connectionsCount());
